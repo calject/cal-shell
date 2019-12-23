40e06f2f05747ea633561df278d7ffece1cdf570
沧澜 <lryhuimeng@163.com>
2019-12-23 11:34:11 +0800

    add command alias prefix name

--- core/action/alias.zsh
+++ core/action/alias.zsh
@@ -7,6 +7,9 @@
 #
 # function:
 # _process:     (_process [message] [type]) 过程输出函数
+# _cmd_alias:   根据当前文件名及(# !alias=xxx,xxx,...)生成alias别名命令,
+#                   必要参数term:           命令执行环境命令(/bin/bash /bin/zsh ...)
+#                   可选参数alias_prefix:   命令别名前缀(默认为 '# !alias=')
 # ========================== 说明 ==========================
 # 执行alias别名声明文件
 # ========================== end ==========================
@@ -14,7 +17,7 @@
 _process "source $file_path" info
 file_content+=("source $file_path")
 for name ($(/usr/bin/sed 's#^alias \([a-zA-Z_-]*\)=.*#\1#gp;d' $file_path | /usr/bin/sed 's/#.*//g')) {
-    [[ $name != '' ]] && {
+    [[ $name ]] && {
         help_content+=("$name:$file_path")
     }
 }
--- core/action/func.zsh
+++ core/action/func.zsh
@@ -7,6 +7,9 @@
 #
 # function:
 # _process:     (_process [message] [type]) 过程输出函数
+# _cmd_alias:   根据当前文件名及(# !alias=xxx,xxx,...)生成alias别名命令,
+#                   必要参数term:           命令执行环境命令(/bin/bash /bin/zsh ...)
+#                   可选参数alias_prefix:   命令别名前缀(默认为 '# !alias=')
 # ========================== 说明 ==========================
 # 执行函数(function)定义文件
 # ========================== end ==========================
@@ -14,7 +17,7 @@
 _process "source $file_path" info
 file_content+=("source $file_path")
 for name ($(/usr/bin/sed "s#\(function\)*[ ]*\([a-zA-Z_-]*\)[ ]*([ ]*)[ {]*.*#\2#gp;d" $file_path)) {
-    [[ $name != '' ]] && {
+    [[ $name ]] && {
         help_content+="$name:$file_path"
     }
 }
