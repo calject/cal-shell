5294325d41bce55a676ce6f504221a058e604a8b
沧澜 <lryhuimeng@163.com>
2019-12-23 11:10:51 +0800

    update shell alias create

--- core/action/shell.zsh
+++ core/action/shell.zsh
@@ -7,26 +7,11 @@
 #
 # function:
 # _process:     (_process [message] [type]) 过程输出函数
+# _cmd_alias:   根据当前文件名及(# !alias=xxx,xxx,...)生成alias别名命令,必要参数term: 命令执行环境命令(/bin/bash /bin/zsh ...)
 # ========================== 说明 ==========================
 # 生成脚本(shell script)别名
 # ========================== end ==========================
 
 command=${$(<${file_path})[(f)1]##*!}
-command_name=${file_path:t:r}
-file_content+=("alias $command_name='${command:-'/bin/bash'} $file_path'")
-_process "alias $command_name='${command:-'/bin/bash'} $file_path'" info
-help_content+=("$command_name:$file_path")
-
-# 查找所有定义别名(# !alias=xxx,xxx,xxx)并生成命令别名
-for str (${(f)"$(<$file_path)"}) {
-	[[ ${(M)str:#\# !alias=*} != '' ]] && {
-	    for alias_name (${=${(s/,/)str##*=}}) {
-		    file_content+="alias $alias_name='${command:-'/bin/bash'} $file_path'"
-		    _process "alias $alias_name='${command:-'/bin/bash'} $file_path'" info
-		    help_content+="$alias_name:$file_path"
-	    }
-	    break
-	}
-}
-
-unset command command_name
+_cmd_alias
+unset command
---
+++ core/script/cmdalias.zsh
@@ -0,0 +1,34 @@
+# ========================== 参数 ==========================
+# 必要全局参数:
+# file_path:    当前处理的脚本或程序文件路径
+# file_content: 写入$model.source文件的内容数组
+# help_content: 命令help文件内容数组,写入格式[command:file_path]
+#
+# 必要执行参数:
+# term: 当前生成的别名命令执行环境命令(例: /bin/bash /bin/zsh /usr/bin/sh ...)
+#
+# function:
+# _process:     (_process [message] [type]) 过程输出函数
+# ========================== 说明 ==========================
+# 根据文件路劲生成alias别名命令
+# ========================== end ==========================
+
+[[ $+term ]] && {
+    command_name=${file_path:t:r}
+    file_content+=("alias $command_name='$term $file_path'")
+    _process "alias $command_name='$term $file_path'" info
+    help_content+=("$command_name:$file_path")
+
+    # 查找所有定义别名(# !alias=xxx,xxx,xxx)并生成命令别名
+    for str (${(f)"$(<$file_path)"}) {
+        [[ ${str} == *!alias=* ]] && [[ ${(M)str:#\# !alias=*} ]] && {
+            for alias_name (${=${(s/,/)str##*=}}) {
+                file_content+="alias $alias_name='$term $file_path'"
+                _process "alias $alias_name='$term $file_path'" info
+                help_content+="$alias_name:$file_path"
+            }
+            break
+        }
+    }
+    unset command_name
+}
