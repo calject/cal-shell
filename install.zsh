0ad0523181b8e363712daad770930257c6b16535
沧澜 <lryhuimeng@163.com>
2019-12-23 10:35:49 +0800

    update install.zsh

--- install.zsh
+++ install.zsh
@@ -6,12 +6,11 @@
 
 # 在当前目录下 clone cal-shell 项目
 git clone https://github.com/calject/cal-shell.git
-
-[[ -d $(pwd)/cal-shell ]] || {
+CAL_HOME=$(pwd)/cal-shell
+[[ -d $CAL_HOME ]] || {
     print -P "%F{red}cal-shell目录不存在.%f"
     exit 1
 }
-cd cal-shell
-git checkout z5.0.2
-source $(pwd)/calbuilder.zsh
+cd $CAL_HOME
+source $CAL_HOME/calbuilder.zsh
 print -P "%F{green}执行[ source $HOME/$shrc_file ]完成.%f"
