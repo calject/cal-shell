b1f84b255c5ec9c63803efc01315675b29474f81
沧澜 <lryhuimeng@163.com>
2020-01-02 17:56:05 +0800

    cahnge calfind.zsh

--- shell/system/calfind.zsh
+++ shell/system/calfind.zsh
@@ -1,8 +1,14 @@
 #!/bin/zsh
 
+# [arg] -v : 显示进程
+# [arg] -h : 显示帮助文档信息
+
+# [help] : 查找项目命令位置 calfind [command]
+
 # !alias=calhelp
-# 查找项目命令位置 calfind [command]
-# 示例: $ calfind calfdind >>> calfind  /Users/canl/cal-shell/shell/system/calfind.sh
+# 示例:
+# $ calfind calfdind
+# $ calfind /Users/canl/cal-shell/shell/system/calfind.sh
 
 local -a content
 local commandName
