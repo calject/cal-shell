3a6ff986df1b759aff6cac622c3381e0f947bd75
沧澜 <lryhuimeng@163.com>
2020-01-07 18:33:33 +0800

    source sys func

--- core/user/script/opts.zsh
+++ core/user/script/opts.zsh
@@ -1,6 +1,8 @@
 
 local -A opts
 
+source $CAL_HOME/core/function/system.func
+
 [[ -f $_path ]] && {
     # ======== 读取命令参数脚本 ========
     while {getopts ${"$(<$CAL_STORAGE/system/opts/$(_md5 $_path).o)"[(f)1]} opt} {
